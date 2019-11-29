e5e960a44ba2d9bf70d0579010c086b9ea508dec
Albert Gomicia <a.gomicia.ubiqum@gmail.com>
2019-11-29 15:52:35 +0100

    First TGIF commit

---
+++ house-data.js
@@ -0,0 +1,134 @@
+//House Data table
+
+var members = data.results[0].members;
+
+function table(membersArray){
+
+    var row = "";
+    var names = "";
+    var party = "";
+    var state = "";
+    var seniority = "";
+    var votes = "";
+
+    var houseData = document.getElementById("house-data");
+
+    houseData.innerHTML = "";
+
+    if(membersArray.length==0){
+        var message = document.createElement("td");
+        message.innerHTML = "SORRY, WE COULDN'T FIND ANY RESULTS MATCHING.";
+        houseData.append(message);
+
+    }else{
+        for(var i=0; i<membersArray.length; i++){
+            row = document.createElement("tr");
+
+            names = document.createElement("td");
+            var a = document.createElement ("a");
+            a.href = membersArray[i].url;
+            if(membersArray[i].middle_name==null){
+                a.innerHTML = membersArray[i].first_name + " " + membersArray[i].last_name;
+            }else{
+                a.innerHTML = membersArray[i].first_name + " " + membersArray[i].middle_name + " " + membersArray[i].last_name;
+            }
+            
+            names.append(a);
+
+            party = document.createElement("td");
+            party.innerHTML = membersArray[i].party;
+
+            state = document.createElement("td");
+            state.innerHTML = membersArray[i].state;
+
+            seniority = document.createElement("td");
+            seniority.innerHTML = membersArray[i].seniority;
+
+            votes = document.createElement("td");
+            votes.innerHTML = membersArray[i].votes_with_party_pct + "%";
+
+            row.append(names, party, state, seniority, votes);
+            houseData.appendChild(row);
+        }
+    }
+}
+
+table(members);
+
+//Filtres
+
+var dCheckbox = document.getElementById("demFilt");
+dCheckbox.addEventListener("click", bothFilters);
+
+var rCheckbox = document.getElementById("repFilt");
+rCheckbox.addEventListener("click", bothFilters);
+
+var iCheckbox = document.getElementById("indFilt");
+iCheckbox.addEventListener("click", bothFilters);
+
+var option = "";
+
+var dropDownList = document.getElementById("stateFilt");
+dropDownList.addEventListener("change", bothFilters);
+
+//Funció (How to determine if object is in array)
+
+function containsObject(obj, list) {
+    var i;
+    for (i = 0; i < list.length; i++) {
+        if (list[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//Drop-Down list amb els States
+
+var stateArray = [];
+
+function fillDropDown (){
+
+    for (var i=0; i<members.length; i++){
+        if(!containsObject(members[i].state, stateArray)){
+            stateArray.push(members[i].state);
+        }
+    }
+
+    stateArray.sort();
+
+    for (var i=0; i<stateArray.length; i++){
+        option = document.createElement("option");
+        option.innerHTML = stateArray[i];
+        option.setAttribute("value", stateArray[i]);
+        dropDownList.append(option);
+    }
+
+}
+fillDropDown();
+
+//Funció que unifica els dos filtres (Party+State)
+
+function bothFilters(){
+
+    var filter = [];
+
+    for (var i=0; i<members.length; i++){
+        if(members[i].state==dropDownList.value || dropDownList.value=="all"){
+            if(members[i].party=="D" && dCheckbox.checked){
+                filter.push(members[i]);
+            }
+            if(members[i].party=="R" && rCheckbox.checked){
+                filter.push(members[i]);
+            }
+            if(members[i].party=="I" && iCheckbox.checked){
+                filter.push(members[i]);
+            }
+            if(!dCheckbox.checked && !rCheckbox.checked && !iCheckbox.checked){
+                filter.push(members[i]);
+            }
+        }
+    }
+    table(filter);
+}
+bothFilters();
---
+++ statisticsHouseLoy.js
@@ -0,0 +1,205 @@
+var statistics = 
+{
+    "Democrats":
+        {
+            "n_reps": 0,
+            "vote_w_party": 0,
+        }
+    ,
+    "Republicans":
+        {
+            "n_reps": 0,
+            "vote_w_party": 0,
+        }
+    ,
+    "Independents":
+        {
+            "n_reps": 0,
+            "vote_w_party": 0,
+        }
+    ,
+    "Total": 
+        {
+            "n_reps": 0,
+            "vote_w_party": 0,
+        }
+}
+
+//HOUSE AT A GLANCE TABLE
+
+var members = data.results[0].members;
+
+function houseGlance(){
+
+    for (var i=0; i<members.length; i++){
+        if(members[i].party=="D"){
+            statistics.Democrats.n_reps += 1;
+        }
+    }
+    for (var i=0; i<members.length; i++){
+        if(members[i].party=="R"){
+            statistics.Republicans.n_reps += 1;
+        }
+    }
+    for (var i=0; i<members.length; i++){
+        if(members[i].party=="I"){
+            statistics.Independents.n_reps += 1;
+        }
+    }
+
+    statistics.Total.n_reps = statistics.Independents.n_reps + statistics.Republicans.n_reps +
+    statistics.Democrats.n_reps;
+
+var votesWPT = 0;
+
+    for (var i=0; i<members.length; i++){
+        votesWPT = votesWPT + members[i].votes_with_party_pct;
+        statistics.Total.vote_w_party = (votesWPT / statistics.Total.n_reps).toFixed(2) + "%";
+    }
+
+var votesWPD = 0;
+
+    for (var i=0; i<members.length; i++){
+        if (members[i].party=="D"){
+            votesWPD = votesWPD + members[i].votes_with_party_pct;
+            statistics.Democrats.vote_w_party = (votesWPD / statistics.Democrats.n_reps).toFixed(2) + "%";
+        }
+    }
+
+var votesWPR = 0;
+
+    for (var i=0; i<members.length; i++){
+        if (members[i].party=="R"){
+            votesWPR = votesWPR + members[i].votes_with_party_pct;
+            statistics.Republicans.vote_w_party = (votesWPR / statistics.Republicans.n_reps).toFixed(2) + "%";
+        }
+    }
+
+var votesWPI =0;
+
+    for (var i=0; i<members.length; i++){
+        if (members[i].party=="I"){
+            votesWPI = votesWPI + members[i].votes_with_party_pct;
+            statistics.Independents.vote_w_party = (votesWPI / statistics.Independents.n_reps).toFixed(2) + "%";
+        }
+    }
+
+//For with an Object to add .js to .html:
+
+    var houseGlance = document.getElementById("HouseAtGla");
+
+    for (var party in statistics){
+        var row = document.createElement("tr");
+        var partyCell = document.createElement("td");
+        partyCell.innerHTML = party;
+        var repsCell = document.createElement("td");
+        repsCell.innerHTML = statistics[party].n_reps;
+        var voteCell = document.createElement("td");
+        voteCell.innerHTML = statistics[party].vote_w_party;
+        row.append (partyCell, repsCell, voteCell);
+        houseGlance.appendChild(row);
+    }
+
+}
+
+houseGlance ();
+
+//HOUSE LOYALTY TABLES
+
+//TOP 10%
+
+function houseLoy (){
+    var row = "";
+    var names = "";
+    var nvotes = "";
+    var pvotes = "";
+    var topMembers=[];
+    var bottomMembers=[];
+
+    var topHouseLoy = document.getElementById("topHouseLoy");
+
+    var sortMembers = Array.from(members);
+    
+    sortMembers.sort(function(a, b){return b.votes_with_party_pct - a.votes_with_party_pct});
+
+    var topTen = Math.round(sortMembers.length / 10);
+
+    for (var i=0; i<topTen; i++){
+        topMembers.push(sortMembers[i]);
+    }
+
+    for (var i=topTen; i<sortMembers.length; i++){
+        if (topMembers[topMembers.length-1].votes_with_party_pct == sortMembers[i].votes_with_party_pct){
+            topMembers.push(sortMembers[i]);
+        }
+    }
+
+    for (var i=0; i<topMembers.length; i++){
+        row = document.createElement("tr");
+
+        names = document.createElement("td");
+        var a = document.createElement("a");
+        a.href = sortMembers[i].url;
+        if(sortMembers[i].middle_name==null){
+            a.innerHTML = sortMembers[i].first_name + " " + sortMembers[i].last_name;
+        }else{
+            a.innerHTML = sortMembers[i].first_name + " " + sortMembers[i].middle_name + " " + sortMembers[i].last_name;
+        }
+        names.append(a);
+
+        nvotes = document.createElement("td");
+        nvotes.innerHTML = sortMembers[i].total_votes;
+
+        pvotes = document.createElement("td");
+        pvotes.innerHTML = sortMembers[i].votes_with_party_pct + "%";
+
+        row.append(names, nvotes, pvotes);
+
+        topHouseLoy.appendChild(row);
+    }
+
+//Bottom 10%
+
+    var bottHouseLoy = document.getElementById ("bottHouseLoy");
+
+    var sortMembersinv = Array.from(members);
+    
+    sortMembersinv.sort(function(a, b){return a.votes_with_party_pct - b.votes_with_party_pct});
+
+    for (var i=0; i<topTen; i++){
+        bottomMembers.push(sortMembersinv[i]);
+    }
+
+    for (var i=topTen; i<sortMembersinv.length; i++){
+        if (bottomMembers[bottomMembers.length-1].votes_with_party_pct == sortMembersinv[i].votes_with_party_pct){
+            bottomMembers.push(sortMembersinv[i]);
+        }
+    }
+
+    for (var i=0; i<bottomMembers.length; i++){
+        row = document.createElement("tr");
+
+        names = document.createElement("td");
+        var a = document.createElement ("a");
+        a.href = sortMembersinv[i].url;
+        if(sortMembersinv[i].middle_name==null){
+            a.innerHTML = sortMembersinv[i].first_name + " " + sortMembersinv[i].last_name;
+        }else{
+            a.innerHTML = sortMembersinv[i].first_name + " " + sortMembersinv[i].middle_name + " " + sortMembersinv[i].last_name;
+        }
+        names.append(a);
+
+        nvotes = document.createElement("td");
+        nvotes.innerHTML = sortMembersinv[i].total_votes;
+
+        pvotes = document.createElement("td");
+        pvotes.innerHTML = sortMembersinv[i].votes_with_party_pct + "%";
+
+        row.append(names, nvotes, pvotes);
+        
+        bottHouseLoy.appendChild(row);
+    }
+
+}
+
+houseLoy ();
